1b28da4a7d1bb47e9eece0e48e59b26812a82ddb
darenlarson <larson.daren@gmail.com>
2019-01-18 11:01:29 -0600

    Some helpers created. route to get project with actions created.

--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -1,10 +1,22 @@
 const express = require('express');
-
 const router = express.Router();
+const db = require('../data/helpers/helpers.js');
 
 // routes
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    
+    db.getProjects(id)
+        .then(project => {
+            res.status(200).json(project);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The project with the specified ID does not exist" })
+        });
+});
+
 router.get('/', (req, res) => {
-    db('gtd')
+    db.getProjects()
         .then(projects => {
             res.status(200).json(projects);
         })
